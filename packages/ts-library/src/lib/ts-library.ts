d2101121c73a96669ed5b91b040a5126707695df
dror <dror.weiss.85@gmail.com>
2022-11-29 14:41:34 +0200

    remove  format:check

--- packages/ts-library/src/lib/ts-library.ts
+++ packages/ts-library/src/lib/ts-library.ts
@@ -2,8 +2,8 @@
  * Check if the passed value is null.
  *
  * @param val
- * @returns `true` whether val is null, `false` otherwise.
+ * @returns `true` if val is null, `false` otherwise.
  */
 export function isNull<T>(val: T | null): val is null {
-  return (val === null);
+  return val === null;
 }
