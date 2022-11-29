b3e0d00a4c20691620068643484d2fb1099a970b
dror <dror.weiss.85@gmail.com>
2022-11-29 15:21:07 +0200

    test release.config.js 4

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,8 +1,3 @@
 module.exports = {
-  '{packages,tools}/**/*.{ts,js,json,md,html,css,scss}': [
-    'nx workspace-lint',
-    'nx affected --target lint --uncommitted --fix true',
-    'nx affected --target test --uncommitted',
-    'nx format:write --uncommitted --libs-and-apps',
-  ],
+
 };
