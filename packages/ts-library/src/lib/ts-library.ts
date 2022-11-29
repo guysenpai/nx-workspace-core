9daeb971c3632e9bb116eb255c3455ed680d927f
dror <dror.weiss.85@gmail.com>
2022-11-29 14:23:20 +0200

    change something

--- packages/ts-library/src/lib/ts-library.ts
+++ packages/ts-library/src/lib/ts-library.ts
@@ -2,7 +2,7 @@
  * Check if the passed value is null.
  *
  * @param val
- * @returns `true` if val is null, `false` otherwise.
+ * @returns `true` whether val is null, `false` otherwise.
  */
 export function isNull<T>(val: T | null): val is null {
   return (val === null);
